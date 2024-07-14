c9e34a364e80e73124c94291c740a65854e4d065
uma <udayarekhachowdary96@gmail.com>
2024-07-14 10:27:01 +0000

    # CP22-Routing-Practice

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="notfound-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png "
+      alt="notfound"
+      className="notfound-img"
+    />
+    <h1 className="notfound-heading">Not Found</h1>
+  </div>
+)
+
+export default NotFound
